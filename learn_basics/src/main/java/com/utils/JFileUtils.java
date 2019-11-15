23e760e016841f7090c1e0d9f2ce26f0d2bf0121
jhs <724033319@qq.com>
2019-11-15 17:41:50 +0800

    处理文件工具 添加新方法 --获取文件夹中所有文件的路径

---
+++ learn_basics/src/main/java/com/utils/JFileUtils.java
@@ -0,0 +1,35 @@
+package com.utils;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 文件工具
+ * 2019-11-15
+ */
+public class JFileUtils {
+    /**
+     * 读取文件中的文件  带路径
+     *
+     * @param path
+     * @return
+     */
+    public static List<String> getFiles(String path) {
+        List<String> files = new ArrayList<String>();
+        File file = new File(path);
+        File[] tempList = file.listFiles();
+
+        for (int i = 0; i < tempList.length; i++) {
+            if (tempList[i].isFile()) {
+                files.add(tempList[i].toString());
+                //文件名，不包含路径
+                //String fileName = tempList[i].getName();
+            }
+            if (tempList[i].isDirectory()) {
+                //这里就不递归了，
+            }
+        }
+        return files;
+    }
+}
